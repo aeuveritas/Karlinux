53952d85a3b0f900297e7d1328dc71de34d35161
aeuveritas <aeuveritas@gmail.com>
2014-12-16 05:53:20 -0800

    Chapter 8] Prepare for Over 1MB for IA-32e

--- kernel32/source/entryPoint.s
+++ kernel32/source/entryPoint.s
@@ -7,10 +7,29 @@ SECTION .text				; Define text section
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 START:
 	mov ax, 0x1000			; 0x10000 is entry point of Protected mode
-	mov ds, ax			; Set DS as 0x1000
-	mov es, ax			; Set ES as 0x1000
+	mov ds, ax				; Set DS as 0x1000
+	mov es, ax				; Set ES as 0x1000
 
-	cli				; Disable interrupt
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Enable A20 Gate
+; If fail, try with system control port
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; Enable A20 Gate with BIOS system
+	mov ax, 0x2401			; Enable A20 Gate
+	int 0x15				; BIOS Interrupt call
+
+	jc .A20GATEERROR			; Check A20 Gate enabled or disabled
+	jmp .A20GATESUCCESS
+
+.A20GATEERROR:
+	; If fail, try with system control port
+	in al, 0x92				; Read system control port 1 byte, and store it at AL
+	or al, 0x02				; AL | 0x02
+	and al, 0xFE			; Prevent reset
+	out 0x92, al			; restore AL to system control port
+
+.A20GATESUCCESS:
+	cli						; Disable interrupt
 	lgdt [ GDTR ] 			; Load GDT table by setting GDTR data structure
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -32,25 +51,25 @@ START:
 [ BITS 32 ]
 PROTECTEDMODE:
 	mov ax, 0x10			; Data segment descripter for protected mode is 0x10
-	mov ds, ax			; Set DS
-	mov es, ax			; Set ES
-	mov fs, ax			; Set FS
-	mov gs, ax			; Set GS
+	mov ds, ax				; Set DS
+	mov es, ax				; Set ES
+	mov fs, ax				; Set FS
+	mov gs, ax				; Set GS
 
 	; Stack is 64KB, 0x00000000 ~ 0x0000FFFF
-	mov ss, ax			; Set SS
+	mov ss, ax				; Set SS
 	mov esp, 0xFFFE			; Set address of ESP as 0xFFFE
 	mov ebp, 0xFFFE			; Set address of EBP as 0xFFFE
 
 	; Print that current mode is protected mode
 	push ( SWITCHSUCCESSMESSAGE - $$ + 0x10000)	
-					; Insert message to stack
-	push 2				; Y axis point
-	push 0				; X axis point
+							; Insert message to stack
+	push 2					; Y axis point
+	push 0					; X axis point
 	call PRINTMESSAGE		; Call PRINTMESSAGE function
-	add esp, 12			; Remove params
+	add esp, 12				; Remove params
 
-	jmp dword 0x08: 0x10200		; 0x10200 is address for C kernel
+	jmp dword 0x08: 0x10200	; 0x10200 is address for C kernel
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	Function Code Section
@@ -58,7 +77,7 @@ PROTECTEDMODE:
 ; Function for message print
 ; PARAM: x, t, message
 PRINTMESSAGE:
-	push ebp			; Insert base point (BP) to stack
+	push ebp				; Insert base point (BP) to stack
 	mov ebp, esp			; Set EBP as ESP
 	push esi		
 	push edi
@@ -70,44 +89,48 @@ PRINTMESSAGE:
 ;	Calculate the video address form X, Y
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Get line address from Y
-	mov eax, dword [ ebp + 12 ]	; Set EAX with parameter 2 (4*(1+2))
+	mov eax, dword [ ebp + 12 ]	
+							; Set EAX with parameter 2 (4*(1+2))
 	mov esi, 160			; Set SI with the numver of byte
-					; as one line
-	mul esi				; Y address = AX * SI
+							; as one line
+	mul esi					; Y address = AX * SI
 	mov edi, eax			; EDI = EAX
 
 ; Get line address from X
-	mov eax, dword [ ebp + 8 ]	; Set EAX with parameter 1 (4*(1+1))
-	mov esi, 2			; Set SI with the numver of byte
-					; as one word
-	mul esi				; X address = AX * SI
+	mov eax, dword [ ebp + 8 ]	
+							; Set EAX with parameter 1 (4*(1+1))
+	mov esi, 2				; Set SI with the numver of byte
+							; as one word
+	mul esi					; X address = AX * SI
 	mov edi, eax			; EDI = EAX
 
 	; Address of print message	
-	mov esi, dword [ ebp + 16 ]	; parameter 3 (4*(1+3))
+	mov esi, dword [ ebp + 16 ]	
+							; parameter 3 (4*(1+3))
 	
-.MESSAGELOOP:                           ; Print message
-        mov cl, byte [ esi ]            ; Copy the value of esi address to CL register.
-                                        ; CL register is low 1 byte of CX register
+.MESSAGELOOP:               ; Print message
+    mov cl, byte [ esi ]    ; Copy the value of esi address to CL register.
+                            ; CL register is low 1 byte of CX register
 
-        cmp cl, 0                       ; if charater is 0, it means end.
-        je .MESSAGEEND                  ; if cl is 0, jump to .MESSAGEEND
+    cmp cl, 0               ; if charater is 0, it means end.
+    je .MESSAGEEND          ; if cl is 0, jump to .MESSAGEEND
 
-        mov byte [ edi + 0xB8000], cl   ; if cl is not 0, print value of edi + 0xB8000 address
+    mov byte [ edi + 0xB8000], cl   
+							; if cl is not 0, print value of edi + 0xB8000 address
 
-        add esi, 1                      ; Move to the next character
-        add edi, 2                      ; Move to the next address
+    add esi, 1              ; Move to the next character
+    add edi, 2              ; Move to the next address
 	
 	jmp .MESSAGELOOP		; Move to the nuext iteration
 
 .MESSAGEEND:
-        pop edx                         ; Pop ES ~ DX from stack
-        pop ecx
-        pop eax
-        pop edi
-        pop esi
-        pop ebp                         ; Restore BP
-        ret                             ; Return to function call
+    pop edx                 ; Pop ES ~ DX from stack
+    pop ecx
+    pop eax
+    pop edi
+    pop esi
+    pop ebp                 ; Restore BP
+    ret                     ; Return to function call
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	Data Section
@@ -120,7 +143,8 @@ dw 0x0000
 ; Define GDTR data structure
 GDTR:
 	dw GDTEND - GDT - 1		; Total size of GDT
-	dd ( GDT - $$ + 0x10000 )	; Start address of GDT
+	dd ( GDT - $$ + 0x10000 )	
+							; Start address of GDT
 
 ; Define GDT
 GDT:
@@ -135,26 +159,27 @@ GDT:
 
 	; Code segment descriptor for protected mode
 	CODEDESCRIPTOR:
-		dw 0xFFFF		; Limit [15:0]
-		dw 0x0000		; Base [15:0]
-		db 0x00			; Base [23:16]
-		db 0x9A			; P=1, DPL=0, Code segment, Execute/Read
-		db 0xCF			; G=1, D=1, L=0, Limit[19:16]
-		db 0x00			; Base [31:24]
+		dw 0xFFFF			; Limit [15:0]
+		dw 0x0000			; Base [15:0]
+		db 0x00				; Base [23:16]
+		db 0x9A				; P=1, DPL=0, Code segment, Execute/Read
+		db 0xCF				; G=1, D=1, L=0, Limit[19:16]
+		db 0x00				; Base [31:24]
 
 	; Data segment descriptor for protected mode
 	DATADESCRIPTOR:
-		dw 0xFFFF		; Limit [15:0]
-		dw 0x0000		; Base [15:0]
-		db 0x00			; Base [23:16]
-		db 0x92			; P=1, DPL=0, Data segment, Read/Write
-		db 0xCF			; G=1, D=1, L=0, Limit[19:16]
-		db 0x00			; Base [31:24]
+		dw 0xFFFF			; Limit [15:0]
+		dw 0x0000			; Base [15:0]
+		db 0x00				; Base [23:16]
+		db 0x92				; P=1, DPL=0, Data segment, Read/Write
+		db 0xCF				; G=1, D=1, L=0, Limit[19:16]
+		db 0x00				; Base [31:24]
 GDTEND:
  
 SWITCHSUCCESSMESSAGE:
 	db 'Switch To Protected Mode Success~!!', 0
 
-times 512 - ( $ - $$ ) db 0x00		; Fill 512 bytes
+times 512 - ( $ - $$ ) db 0x00		
+							; Fill 512 bytes
 
 	
--- kernel32/source/main.c
+++ kernel32/source/main.c
@@ -1,11 +1,41 @@
 #include "types.h"
 
-void kPrintSting( int ix, int iY, const char* pcString );
+void kPrintString( int ix, int iY, const char* pcString );
+BOOL kInitializeKernel64Area( void );
+BOOL kIsMemoryEnough( void );
 
 // Main function
 void main(void)
 {
-	kPrintString( 0, 3, "C Language Kernel Started~!!!" );
+	DWORD i;
+	
+	kPrintString( 0, 0, "                                                          " );
+	kPrintString( 0, 0, "C Language Kernel Start.....................[Pass]" );
+
+	// Check minimum memory size
+	kPrintString( 0, 1, "Minimum Memory Size Check...................[    ]" );
+	if (kIsMemoryEnough() == FALSE )
+	{
+		kPrintString( 45, 1, "Fail" );
+		kPrintString( 0, 2, "Not Enough Memory!! Karlinux OS Requires Over "
+				"64MB Memory!!" );
+		while ( 1 );
+	}
+	else
+	{
+		kPrintString( 45, 1, "Pass" );
+	}
+
+	// Initialize kernel area for IA-32e mode
+	kPrintString( 0, 2, "IA-32e kernel Area Initialize...............[    ]" );
+	if ( kInitializeKernel64Area() == FALSE )
+	{
+		kPrintString( 45, 2, "Fail" );
+		kPrintString( 0, 3, "Kernel Area Initialization Fail!!" );
+
+		while ( 1 );
+	}
+	kPrintString( 45, 2, "Pass" );
 
 	while( 1 );
 }
@@ -24,3 +54,56 @@ void kPrintString( int iX, int iY, const char* pcString )
 	}
 }
 
+// Initialize kernel area for IA-32e
+BOOL kInitializeKernel64Area( void )
+{
+	DWORD* pdwCurrentAddress;
+
+	// Initial start point is 0x10000 (1MB)
+	pdwCurrentAddress = ( DWORD* ) 0x100000;
+
+	// Initial end point is 0x60000 (6MB)
+	while ( ( DWORD ) pdwCurrentAddress < 0x600000 )
+	{
+		*pdwCurrentAddress = 0x00;
+
+		// After write 0x00 at pdwCurrentAddress,
+		// If the value is not 0, there is a problem
+		if (*pdwCurrentAddress != 0 )
+		{
+			return FALSE;
+		}
+		
+		// Go to next address
+		pdwCurrentAddress++;
+	}
+
+	return TRUE;
+}
+
+// Check miminum memory size
+BOOL kIsMemoryEnough( void )
+{
+	DWORD* pdwCurrentAddress;
+
+	// Start form 0x100000 (1MB)
+	pdwCurrentAddress = ( DWORD* ) 0x100000;
+
+	// Til 0x4000000 (64MB)
+	while ( ( DWORD ) pdwCurrentAddress < 0x4000000 )
+	{
+		*pdwCurrentAddress = 0x12345678;
+
+		// After write 0x12345678 at pdwCurrentAddress,
+		// If the value is not 0x12345678, there is a problem
+		if ( *pdwCurrentAddress != 0x12345678 )
+		{
+			return FALSE;
+		}
+
+		// Go to next 1MB
+		pdwCurrentAddress += ( 0x100000 / 4 ); // 4 byte = 32 bit
+	}
+
+	return TRUE;
+}
